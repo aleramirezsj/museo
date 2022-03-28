17ec61dc97b7a716d8e1311199a92b3070119a95
Rubén Alejandro Ramirez <aleramirezsj@gmail.com>
2022-03-28 11:02:47 -0300

    Initial commit

---
+++ data.js
@@ -0,0 +1,266 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-patio",
+      "name": "Patio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": 1.683683082034321,
+        "pitch": 0.1399313746998363,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0855679254781716,
+          "pitch": 0.23166539231407057,
+          "rotation": 0,
+          "target": "1-entrada-museo"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7492883746529868,
+          "pitch": 0.0271426305292497,
+          "title": "<span id=\"docs-internal-guid-318d12c1-7fff-17ca-40dc-87c54447df4f\"><span style=\"font-size: 11pt; font-family: Calibri, sans-serif; color: rgb(0, 0, 0); background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;\">&nbsp;Museo Histórico Municipal “Rosa Giussani de Piva”</span></span>",
+          "text": "<span id=\"docs-internal-guid-33cc4d84-7fff-e10e-64a0-696a627a2ba3\"><span style=\"font-size: 11pt; font-family: Calibri, sans-serif; color: rgb(0, 0, 0); background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;\">Este antiguo inmueble no sólo fue sede de la Escuela Normal de San Justo desde 1910 hasta 1981, sino que anteriormente ya existía.</span></span><br>"
+        },
+        {
+          "yaw": 2.558155918723548,
+          "pitch": 0.022187612561470615,
+          "title": "En el patio",
+          "text": "<span id=\"docs-internal-guid-bd83b3bc-7fff-181f-6766-6911c57463fd\"><span style=\"font-size: 11pt; font-family: Calibri, sans-serif; color: rgb(0, 0, 0); background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;\">&nbsp;En 1884, fue la Sede de la Subdelegación Política de las Colonias y Campañas del Norte y allí vivió el Coronel Francisco Romero Manzanares y su Guardia de Seguridad.</span></span>"
+        },
+        {
+          "yaw": -2.900737318163708,
+          "pitch": 0.03429992539329163,
+          "title": "El edificio",
+          "text": "<span id=\"docs-internal-guid-842b709e-7fff-8776-a1a1-53d723fa1b16\"><p dir=\"ltr\" style=\"line-height:1.295;text-align: justify;margin-top:12pt;margin-bottom:12pt;\"><span style=\"color: rgb(0, 0, 0); font-family: Calibri, sans-serif; font-size: 14.6667px; white-space: pre-wrap;\">Declarado Patrimonio Histórico Cultural de la Provincia en el año 2012.</span><br></p></span>"
+        },
+        {
+          "yaw": -2.1268353284234536,
+          "pitch": 0.03012649984799154,
+          "title": "Activo cultural",
+          "text": "<span id=\"docs-internal-guid-4e5b0664-7fff-b94a-3663-fa296f4cba2b\"><span style=\"font-size: 11pt; font-family: Calibri, sans-serif; color: rgb(0, 0, 0); background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;\">Posee una colección de más de 3000 elementos, testimonios esenciales del paso del tiempo en San Justo. Ofrece 5 salas donde las historias locales son la base de nuestra identidad cultural.</span></span>"
+        }
+      ]
+    },
+    {
+      "id": "1-entrada-museo",
+      "name": "Entrada Museo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3212.5,
+      "initialViewParameters": {
+        "yaw": -2.3601260815180023,
+        "pitch": 0.21749760879997027,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.555725267104126,
+          "pitch": 0.046963289405683284,
+          "rotation": 0,
+          "target": "2-sala-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.379502759203344,
+          "pitch": -0.06059909628427107,
+          "title": "Museo",
+          "text": "<span id=\"docs-internal-guid-31876fc1-7fff-b2a8-b278-270bf3ab845e\"><span style=\"font-size: 11pt; font-family: Calibri, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;\">El 24 de septiembre de 1987, por iniciativa de una Comisión formada por ciudadanos representantes de distintas instituciones y descendientes de los primeros inmigrantes, y con el apoyo del Intendente Municipal de entonces – el Ing. Héctor Scotta-, nace el Museo Histórico Municipal “Rosa Giussani de Piva”, en calle 9 de julio 2457. </span></span><span id=\"docs-internal-guid-9e4d1f92-7fff-e7f5-6f6e-a4121f82182f\"><span style=\"font-size: 11pt; font-family: Calibri, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;\">En dicho lugar, funcionó la institución hasta el año 2018, año en el que se concretó el traslado a la Casa de la Cultura.</span></span>"
+        },
+        {
+          "yaw": -2.8410860642120603,
+          "pitch": -0.05740505332327217,
+          "title": "<span id=\"docs-internal-guid-e638ea97-7fff-b10f-5f74-205aa9f0e2f1\"><span style=\"font-size: 12pt; font-family: Calibri, sans-serif; background-color: transparent; font-weight: 700; font-style: italic; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;\">Museo Histórico Municipal “Rosa Giussani de Piva”</span></span>",
+          "text": "<span id=\"docs-internal-guid-17073b43-7fff-e9e7-d221-11b8ee3c7bec\"><span style=\"font-size: 11pt; font-family: Calibri, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;\">Lleva su nombre en homenaje a Doña Rosa Giussani, maestra ejemplar de la comunidad, quien dona su casa para fines culturales.</span></span><div><span id=\"docs-internal-guid-121ef0e8-7fff-041f-da1a-f3fb486c4f9d\"><span style=\"font-size: 11pt; font-family: Calibri, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;\"><span style=\"border:none;display:inline-block;overflow:hidden;width:180px;height:241px;\"><img src=\"https://lh3.googleusercontent.com/tbInDTcwqLiOuMqiGMcSo_qgZoUIYdQlYf5I1Y5PHf3BeLn0hEZ84lC7iplMYWGJFr5ms-xERZltE-jVxt9JTxGGUpgxQbYQ_wppMGS60TlmNJyPKvXrDelNW-D5I7Vobnpjitgh\" width=\"180\" height=\"241\" style=\"margin-left:0px;margin-top:0px;\"></span></span></span><span><span style=\"font-size: 11pt; font-family: Calibri, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;\"><br></span></span></div>"
+        }
+      ]
+    },
+    {
+      "id": "2-sala-1",
+      "name": "Sala 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3315.5,
+      "initialViewParameters": {
+        "yaw": -2.695963404440814,
+        "pitch": 0.16099556766123335,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7579995830621993,
+          "pitch": 0.09101602256229846,
+          "rotation": 1.5707963267948966,
+          "target": "3-sala-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.947088183202684,
+          "pitch": 0.016497104620601277,
+          "title": "Orígenes",
+          "text": "<span id=\"docs-internal-guid-eabf45da-7fff-c85a-cfa2-66f073262c9b\"><span style=\"font-size: 11pt; font-family: Calibri, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;\">Los objetos nos cuentan de los inicios del Museo, a quien se debe su nombre, quien fue Doña Rosa Giussani de Piva.</span></span><br>"
+        },
+        {
+          "yaw": -1.0652911135608925,
+          "pitch": 0.03322619486767664,
+          "title": "Restos fósiles",
+          "text": "<span id=\"docs-internal-guid-0e49f1e5-7fff-910d-fd25-8c1eb5863940\" style=\"font-size: 14.6667px; white-space: pre-wrap;\"><p dir=\"ltr\" style=\"line-height:1.38;margin-top:0pt;margin-bottom:0pt;\"><span id=\"docs-internal-guid-eb21746a-7fff-38ca-1f84-ac98ddefecbb\"><span style=\"font-size: 11pt; font-family: Calibri, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline;\">Encontrados a la vera del río Salado, evidenciando los principios de la vida en nuestra zona.</span></span><br></p></span>"
+        },
+        {
+          "yaw": 1.5250581213731618,
+          "pitch": 0.09480444571127045,
+          "title": "<span id=\"docs-internal-guid-00c90b47-7fff-45ea-175b-eb78a212596a\"><span style=\"font-size: 14pt; font-family: Calibri, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;\">Primeros pobladores</span></span>",
+          "text": "<span id=\"docs-internal-guid-7e332105-7fff-ba26-32b3-97d8f8f9ba2f\"><span style=\"font-size: 11pt; font-family: Calibri, sans-serif; color: rgb(239, 239, 239); background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;\">Fragmentos de cerámica pertenecientes a los Primeros pobladores de nuestras tierras.</span></span><br>"
+        }
+      ]
+    },
+    {
+      "id": "3-sala-2",
+      "name": "Sala 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.09668527379880487,
+        "pitch": -0.04948037845323938,
+        "fov": 1.3417914392319403
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.1385947963542904,
+          "pitch": -0.18874180729140377,
+          "title": "<span id=\"docs-internal-guid-58c56604-7fff-b95e-e7af-419dc8d6e611\"><span style=\"font-size: 11pt; font-family: Calibri, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;\">Colonia y pueblo de San Justo</span></span>",
+          "text": "<span id=\"docs-internal-guid-811e92fb-7fff-26c4-92f5-bc862b222e74\"><span style=\"font-size: 11pt; font-family: Calibri, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;\">Las piezas exhibidas nos hablan de la llegada de los inmigrantes, el establecimiento de la Colonia, la fundación del pueblo, su organización y crecimiento, y de los pioneros protagonistas de esas acciones.</span></span>"
+        },
+        {
+          "yaw": -2.625694340762058,
+          "pitch": -0.12293875874431848,
+          "title": "<span id=\"docs-internal-guid-cde6158b-7fff-7941-ebe3-d2311a9b06ef\"><span style=\"font-size: 11pt; font-family: Calibri, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;\">La llegada de los inmigrantes</span></span>",
+          "text": ""
+        },
+        {
+          "yaw": 2.9038555920880302,
+          "pitch": 0.15293997022031824,
+          "title": "<span id=\"docs-internal-guid-6bc22e0c-7fff-0b77-85cb-e3a49e6617ac\"><span style=\"font-size: 11pt; font-family: Calibri, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;\">Documentación de los inmigrantes</span></span>",
+          "text": ""
+        },
+        {
+          "yaw": 1.907948287001842,
+          "pitch": 0.021491811073110156,
+          "title": "Cnel Francisco Manzanares",
+          "text": "<span id=\"docs-internal-guid-d9ace615-7fff-d53b-68b1-eaa1bb03143b\"><span style=\"font-size: 11pt; font-family: Calibri, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;\">Sable que perteneció a Francisco Romero Manzanares. Fue entregado en 1893 a Pascual Aste -dueño del primer Negocio de Ramos Generales-, ante el inminente avance de los pueblos originarios asentados en las inmediaciones del Río Salado.</span></span>"
+        },
+        {
+          "yaw": 1.5639693380025248,
+          "pitch": 0.23392145480030635,
+          "title": "<span id=\"docs-internal-guid-5119277d-7fff-1f5d-4973-6650b38ed213\"><span style=\"font-size: 11pt; font-family: Calibri, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;\">Cimientos de la antigua comandancia.</span></span>",
+          "text": ""
+        },
+        {
+          "yaw": 1.59013185336683,
+          "pitch": -0.12804648321101375,
+          "title": "<span id=\"docs-internal-guid-674c807c-7fff-92bf-1302-d7158116b068\"><span style=\"font-size: 11pt; font-family: Calibri, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;\">Fundación de San Justo.</span></span>",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
